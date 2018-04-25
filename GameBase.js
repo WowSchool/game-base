cd0ad134f2b15c22fa0aa4673f83634eedf7abf0
wangweihua <aztack@163.com>
2018-04-25 15:22:28 +0800

    add $xhrGet method

--- GameBase.js
+++ GameBase.js
@@ -1,37 +1,79 @@
-/* 不要动修改这个文件! */
 const env = {
   a: !!location.hostname.match(/^(a[0-9]+)-/),
   local: !!location.hostname.match(/^(test)-/),
   prod: !!!location.hostname.match(/^(a[0-9]+|test)-/),
   lang: (window.navigator.language || window.browserLanguage)
-}
+};
 
+/**
+ * Base Game class
+ */
 cc.Class({
   extends: cc.Component,
-  properties: {
-  },
-  onLoad () {
+  properties: {},
+  onLoad: function () {
     this._env = env;
     this._params = parseQuery(location.query);
-    console.log('Loading ...')
+    this.$debug('Loading ...');
   },
-  $debug () {
-    if (this._env.prod) return
-    console.log.apply(console, arguments)
+  $debug: function () {
+    if (this._env.prod) return;
+    console.log.apply(console, arguments);
   },
-  $log () {
-    console.log.apply(console, arguments)
-  }
+  $log: function () {
+    console.log.apply(console, arguments);
+  },
+  $xhrGet: xhrGet
 });
 
 /**
+ * HTTP request throught XHR
+ * @param  {String} url
+ * @param  {Object} opts {timeout,dataType}
+ * @return {Promise}
+ */
+function xhrGet (url, opts) {
+  if (typeof opts === 'undefined') opts = {};
+
+  const xhr = cc.loader.getXMLHttpRequest();
+  xhr.open('GET', url, true);
+  if (cc.sys.isNative) xhr.setRequestHeader("Accept-Encoding", "gzip,deflate");
+
+  // set timeout
+  xhr.timeout = 60000;
+  if (!isNaN(parseInt(opts.timeout))) xhr.timeout = opts.timeout;
+
+  // set
+  xhr.send();
+  return new Promise(function (resolve, reject) {
+    xhr.onreadystatechange = function () {
+      try {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+          if (xhr.status === 200) {
+            var result = xhr.responseText;
+            if (opts.dateType === 'json') {
+              result = JSON.parse(result)
+            }
+            resolve(result);
+          } else {
+            reject(xhr);
+          }
+        }
+      } catch (e) {
+        reject(e);
+      }
+    }
+  })
+}
+
+/**
  * Parse query string
- * @param {string} query 
+ * @param {string} query
  */
 function parseQuery (query) {
   var res = {};
   query = query.trim().replace(/^(\?|#|&)/, '');
-  if (!query) return res
+  if (!query) return res;
 
   query.split('&').forEach(function (param) {
     var parts = param.replace(/\+/g, ' ').split('=');
@@ -48,5 +90,5 @@ function parseQuery (query) {
       res[key] = [res[key], val];
     }
   });
-  return res
+  return res;
 }
