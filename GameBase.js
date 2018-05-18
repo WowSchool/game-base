47dff27a8cf3e605bfa9806d40212286f249ce6b
wangweihua <aztack@163.com>
2018-05-18 21:19:26 +0800

    add 2 prefab methods

--- GameBase.js
+++ GameBase.js
@@ -1,11 +1,45 @@
 const env = require('util-env');
 const strings = require('util-strings');
 const xhr = require('util-xhr');
-
+var mask = null;
 const methods = {
   $debug: env.debug,
   $log: env.log,
-  $xhrGet: xhr.get
+  $xhrGet: xhr.get,
+  /**
+   * Instantiate a cc.Node from a Prefab
+   * @param {String} dir directory name in assets/resources/<dir>
+   * @param {String} name prefab name in <dir>
+   */
+  addPrefabToScene (dir, name = dir, compName = name) {
+    return this.instantiatePrefab(dir, name).then(node => {
+      const scene = cc.director.getScene();
+      const canvas = scene.children[0];
+      node.x = canvas.width / 2;
+      node.y = canvas.height / 2;
+      node.active = false;
+      const comp = node.addComponent(name);
+      scene.addChild(node);
+      return comp;
+    });
+  },
+  /**
+   * Instantiate a cc.Node from a Prefab
+   * @param {String} dir directory name in assets/resources/<dir>
+   * @param {String} name prefab name in <dir>
+   */
+  instantiatePrefab (dir, name = dir) {
+    return new Promise((resolve, reject) => {
+      cc.loader.loadRes(`${dir}/${name}`, function (err, prefab) {
+        if (err) {
+          reject(err);
+        } else {
+          const node = cc.instantiate(prefab);
+          resolve(node);
+        }
+      });
+    });
+  }
 }
 /**
  * Base Game class
