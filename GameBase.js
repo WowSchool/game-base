ce15880069fdfd06b54fe9af3fa1beb12a45bf7d
wangweihua <aztack@163.com>
2018-05-18 14:49:45 +0800

    inject methods into components whose scenceScript property is true

--- GameBase.js
+++ GameBase.js
@@ -1,6 +1,12 @@
 const env = require('util-env');
 const strings = require('util-strings');
 const xhr = require('util-xhr');
+
+const methods = {
+  $debug: env.debug,
+  $log: env.log,
+  $xhrGet: xhr.get
+}
 /**
  * Base Game class
  */
@@ -9,9 +15,15 @@ cc.Class({
   onLoad: function () {
     this._env = env;
     this._params = strings.parseQuery(location.query);
-    this.$debug('Loading ...');
+    this.init();
   },
-  $debug: env.debug,
-  $log: env.log,
-  $xhrGet: xhr.get
+  init () {
+    const comps = this.node._components;
+    for (let i in comps) {
+      let comp = comps[i];
+      if (!comp || comp.sceneScript !== true) continue;
+      // inject methods into components whose scenceScript property is true
+      Object.assign(comp, methods);
+    }
+  }
 });
